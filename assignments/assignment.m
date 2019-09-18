6034de23e634b136d0ebf2a1d4181c3fe60d29cc
Zach Siegel <zsiegel92@gmail.com>
2019-09-18 10:59:49 -0700

    matlab code

--- assignments/assignment.m
+++ assignments/assignment.m
@@ -8,7 +8,11 @@ xvals = xvals';
 n = length(xvals);
 yvals = beta*xvals + alpha + randn(n,1);
 yvals = yvals;
-
 X = [ones(n,1),xvals];
 OLS = inv((X')*X)*(X')*yvals;
-plot(xvals,yvals)
+% alpha_hat = OLS(1);
+% beta_hat = OLS(2);
+y_estimates = X*OLS;
+plot(xvals,yvals,'b^',xvals,y_estimates,'r--')
+title('Data Normally Distributed About a Line and OLS Trendline')
+legend({'observed','predicted'},'Location','northwest')
