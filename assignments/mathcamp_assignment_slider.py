f009fa9a67e6df1d1471c99cb2628d6b49586887
Zach Siegel <zsiegel92@gmail.com>
2019-09-18 00:44:51 -0700

    url for binder

--- assignments/mathcamp_assignment_slider.py
+++ assignments/mathcamp_assignment_slider.py
@@ -1,4 +1,4 @@
-#visit https://mybinder.org/v2/gh/zsiegel92/mathcamp/master?filepath=assignments%2FMath%20Camp%20Assignment.ipynb
+#visit https://mybinder.org/v2/gh/zsiegel92/mathcamp/master?filepath=assignments%2FMath%20Camp%20Assignment%20with%20Slider.ipynb
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from numpy.random import normal as randn
